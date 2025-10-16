3e2dd6d77889a66b4ee9da552ede5d0c000c18d5
mariomancebom <mario.mancebo@alumnos.upm.es>
2025-10-16 11:28:32 +0200

    Initial commit

---
+++ src/es/upm/dit/aled/clase16oct/BubbleSort.java
@@ -0,0 +1,27 @@
+package es.upm.dit.aled.clase16oct;
+
+public class BubbleSort {
+
+	private static void bubbleSort(int[] list) {
+		for (int i = 0; i < list.length; i++) {
+			for (int j = 0; j < list.length - 1 -i; j++) {
+				// Si es mayor el segundo lo cambiamos
+				if (list[j] > list[j + 1]) {
+					// Permutacion de dos numeros
+					int temp = list[j];
+					list[j] = list[j + 1];
+					list[j + 1] = temp;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] list = { 31, 29, 27, 25, 23, 21, 19, 17, 15, 13, 11, 9, 7, 5, 3, 1 };
+		bubbleSort(list);
+		System.out.println("Ordenado:");
+		for (int v : list) {
+			System.out.println(v);
+		}
+	}
+}
