4ab6674f4a9a59e56fccf1e935644426b4d48367
mariomancebom <mario.mancebo@alumnos.upm.es>
2025-11-06 19:27:22 +0100

    Actualizado 6/11

--- src/es/upm/dit/aled/clase16oct/MargeSort.java
+++ src/es/upm/dit/aled/clase16oct/MargeSort.java
@@ -5,86 +5,25 @@ import java.util.Arrays;
 public class MargeSort {
 
 	private static int[] margeSort(int[] list) {
-		//caso base 
-		if(list.length==1) {
+		// caso base
+		if (list.length == 1) {
 			return list;
 		}
-		//Paso recursivo 
-		int[] left=Arrays.copyOfRange(list, 0, list.length/2);
-		int[] right=Arrays.copyOfRange(list, list.length/2, list.length);
-		//Ordeno la mitad izquierda
-		int[] sortedLeft=margeSort(left);
-		//Ordeno la mitad derecha
-		int[] sortedRight=margeSort(right);
-		//Creo el array en el que pondre las cosas ordenadas 
-		int[] sortedMarged= new int[list.length];
-		int iMerged=0, iLeft=0,iRight=0;
-		while(iLeft<sortedLeft.length || iRight <sortedRight.length) {
-			if(sortedLeft[iLeft] <= sortedRight[iLeft]) {
-				//Cojo del array izquierdo 
-				
-				return null;
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
-				
+		// Paso recursivo
+		int[] left = Arrays.copyOfRange(list, 0, list.length / 2);
+		int[] right = Arrays.copyOfRange(list, list.length / 2, list.length);
+		// Ordeno la mitad izquierda
+		int[] sortedLeft = margeSort(left);
+		// Ordeno la mitad derecha
+		int[] sortedRight = margeSort(right);
+		// Creo el array en el que pondre las cosas ordenadas
+		int[] sortedMarged = new int[list.length];
+		int iMerged = 0, iLeft = 0, iRight = 0;
+		while (iLeft < sortedLeft.length || iRight < sortedRight.length) {
+			if (sortedLeft[iLeft] <= sortedRight[iLeft]) {
+				// Cojo del array izquierdo
 			}
-
 		}
-
+		return null;
 	}
 }
